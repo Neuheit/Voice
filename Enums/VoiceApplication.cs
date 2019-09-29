da68d3e5f5de9e7fcabc320dcb6bbf4dc50ab734
Yucked <vuxdev@gmail.com>
2019-09-28 18:11:57 -0700

    Opus voice application

---
+++ Enums/VoiceApplication.cs
@@ -0,0 +1,23 @@
+namespace Vysn.Voice.Enums
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum VoiceApplication
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Voice = 2048,
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        Music = 2049,
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        LowLatency = 2051
+    }
+}
