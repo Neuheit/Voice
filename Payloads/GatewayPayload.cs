65e7d0d2c1fc8325ceabda2de682ca6445f0d7b1
Yucked <vuxdev@gmail.com>
2019-10-08 20:07:57 -0700

    Op wasn't being set

--- Payloads/GatewayPayload.cs
+++ Payloads/GatewayPayload.cs
@@ -1,32 +1,14 @@
-using System;
 using System.Text.Json.Serialization;
 using Vysn.Voice.Enums;
 
 namespace Vysn.Voice.Payloads
 {
-    internal sealed class GatewayPayload<T>
+    internal struct GatewayPayload<T>
     {
         [JsonPropertyName("op")]
-        public VoiceOpCode Op { get; }
+        public VoiceOpCode Op { get; set; }
 
         [JsonPropertyName("d")]
         public T Data { get; set; }
-
-        public GatewayPayload()
-        {
-            Op = Data switch
-            {
-                long _                      => VoiceOpCode.Heartbeat,
-                HelloPayload _              => VoiceOpCode.Hello,
-                ResumeConnectionPayload _   => VoiceOpCode.Resume,
-                ResumedPayload _            => VoiceOpCode.Resumed,
-                SelectProtocolPayload _     => VoiceOpCode.SelectProtocol,
-                SessionDescriptionPayload _ => VoiceOpCode.SessionDescription,
-                SpeakingPayload _           => VoiceOpCode.Speaking,
-                VoiceIdentifyPayload _      => VoiceOpCode.Identify,
-                VoiceReadyPayload _         => VoiceOpCode.Ready,
-                _                           => throw new ArgumentException("Failed to match data to operation type!")
-            };
-        }
     }
 }
